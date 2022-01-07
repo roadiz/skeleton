204037acb34c9a4c52a553add09d24ec9dfd5fa1
Ambroise Maupate <ambroise@rezo-zero.com>
2022-01-07 10:36:26 +0100

    Comment out before_launch

--- docker/php-nginx-alpine/before_launch.sh
+++ docker/php-nginx-alpine/before_launch.sh
@@ -14,11 +14,12 @@ printenv | sed 's/^\(.*\)$/export \1/g' | grep -E "^export (IR|SYMFONY|APP|ROADI
 /usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/console cache:clear -n"
 
 # Uncomment following line to enable automatic migration for your theme at each docker start
-/usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/console doctrine:migrations:migrate -q --allow-no-migration"
 
-if [ -e "./src/Resources/config.yml" ]; then
-  /usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/console themes:migrate -n src/Resources/config.yml"
-fi
-
-/usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/console assets:install -n"
-/usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/console themes:assets:install -n Rozier"
+#/usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/console doctrine:migrations:migrate -q --allow-no-migration"
+#
+#if [ -e "./src/Resources/config.yml" ]; then
+#  /usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/console themes:migrate -n src/Resources/config.yml"
+#fi
+#
+#/usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/console assets:install -n"
+#/usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/console themes:assets:install -n Rozier"
