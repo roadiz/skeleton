b6484442cae62cffea26a57a5409382655563ca0
Ambroise Maupate <ambroise@rezo-zero.com>
2022-02-09 11:20:02 +0100

    chore: removed project_env usage

--- docker/php-nginx-alpine/before_launch.sh
+++ docker/php-nginx-alpine/before_launch.sh
@@ -7,16 +7,16 @@
 /bin/sleep 2s;
 
 # Print local env vars to .env.xxx.php file for performances and crontab jobs
-/usr/bin/sudo -u www-data -- bash -c  "/var/www/html/project_env.sh; /usr/local/bin/composer dump-env prod"
-/usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/console cache:clear -n"
+/usr/bin/sudo -u www-data -- bash -c  "/usr/local/bin/composer dump-env prod"
+/usr/bin/sudo -u www-data -- bash -c "/var/www/html/bin/console cache:clear -n"
 
 # Uncomment following line to enable automatic migration for your theme at each docker start
 
-#/usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/console doctrine:migrations:migrate -q --allow-no-migration"
+#/usr/bin/sudo -u www-data -- bash -c "/var/www/html/bin/console doctrine:migrations:migrate -q --allow-no-migration"
 #
 #if [ -e "./src/Resources/config.yml" ]; then
-#  /usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/console themes:migrate -n src/Resources/config.yml"
+#  /usr/bin/sudo -u www-data -- bash -c "/var/www/html/bin/console themes:migrate -n src/Resources/config.yml"
 #fi
 #
-#/usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/console assets:install -n"
-#/usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/console themes:assets:install -n Rozier"
+#/usr/bin/sudo -u www-data -- bash -c "/var/www/html/bin/console assets:install -n"
+#/usr/bin/sudo -u www-data -- bash -c "/var/www/html/bin/console themes:assets:install -n Rozier"
