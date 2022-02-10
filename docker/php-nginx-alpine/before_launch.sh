9568a04d861f7be22f5599b4d0007058a2f9d891
Ambroise Maupate <ambroise@rezo-zero.com>
2022-02-10 16:03:58 +0100

    Update before_launch.sh

--- docker/php-nginx-alpine/before_launch.sh
+++ docker/php-nginx-alpine/before_launch.sh
@@ -10,6 +10,9 @@
 /usr/bin/sudo -u www-data -- bash -c  "/usr/local/bin/composer dump-env prod"
 /usr/bin/sudo -u www-data -- bash -c "/var/www/html/bin/console cache:clear -n"
 
+/usr/bin/sudo -u www-data -- bash -c "/var/www/html/bin/console assets:install -n"
+/usr/bin/sudo -u www-data -- bash -c "/var/www/html/bin/console themes:assets:install -n Rozier"
+
 # Uncomment following line to enable automatic migration for your theme at each docker start
 
 #/usr/bin/sudo -u www-data -- bash -c "/var/www/html/bin/console doctrine:migrations:migrate -q --allow-no-migration"
@@ -17,6 +20,3 @@
 #if [ -e "./src/Resources/config.yml" ]; then
 #  /usr/bin/sudo -u www-data -- bash -c "/var/www/html/bin/console themes:migrate -n src/Resources/config.yml"
 #fi
-#
-#/usr/bin/sudo -u www-data -- bash -c "/var/www/html/bin/console assets:install -n"
-#/usr/bin/sudo -u www-data -- bash -c "/var/www/html/bin/console themes:assets:install -n Rozier"
