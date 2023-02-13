71642086cf28b342f5089a40850eb1f871f2cc9a
Ambroise Maupate <ambroise@rezo-zero.com>
2023-02-13 12:21:30 +0100

    fix: added isTransactional: false on Doctrine migrations

--- migrations/Version20221206171114.php
+++ migrations/Version20221206171114.php
@@ -28,4 +28,9 @@ final class Version20221206171114 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE messenger_messages');
     }
+    
+    public function isTransactional(): bool
+    {
+        return false;
+    }
 }
