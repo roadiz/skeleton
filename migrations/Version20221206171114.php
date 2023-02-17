e594113ae15dcd7cbe813eca1ac295bb3b16cde1
Ambroise Maupate <ambroise@rezo-zero.com>
2023-02-17 15:09:20 +0100

    fix: skip messenger_messages migration if table already exists

--- migrations/Version20221206171114.php
+++ migrations/Version20221206171114.php
@@ -19,16 +19,16 @@ final class Version20221206171114 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
+        $this->skipIf($schema->hasTable('messenger_messages'), 'messenger_messages table already exists.');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
+        $this->skipIf(!$schema->hasTable('messenger_messages'), 'messenger_messages table does not exist.');
         $this->addSql('DROP TABLE messenger_messages');
     }
-    
+
     public function isTransactional(): bool
     {
         return false;
